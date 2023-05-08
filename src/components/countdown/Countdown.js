954e549b57b7ba60daf6d7f1bdb6780c280c18b6
Pierre <pierrefraisse90@gmail.com>
2023-05-08 16:19:37 +0200

    countdown styling

--- src/components/countdown/Countdown.js
+++ src/components/countdown/Countdown.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import'./Countdown'
+import'./Countdown.scss'
 
 const Countdown = () => {
   return (
-    <div>
-      
+    <div className='countdown-section'>
+      <div className="container --flex-between">
+        <h2 className='--text-md'>Christmas Sale</h2>
+        <div className="countdown --card --my">
+          <time className="--flex-center --text-center --color-white --p">
+            <span>
+              <p>00</p>
+              <small>Days</small>
+            </span>
+            <span>
+              <p>01</p>
+              <small>Hours</small>
+            </span>
+            <span>
+              <p>20</p>
+              <small>Minutes</small>
+            </span>
+            <span>
+              <p>50</p>
+              <small>Seconds</small>
+            </span>
+          </time>
+
+        </div>
+      </div>
+      <p className='close'>x</p>
     </div>
   )
 }
